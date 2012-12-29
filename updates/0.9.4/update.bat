6d1d5743d858c8f11c200a1ffe0ed1ce58abd43e
ddp2p <msilaghi@fit.edu>
2012-12-29 11:39:47 -0500

    release 0.9.4

---
+++ updates/0.9.4/update.bat
@@ -0,0 +1,29 @@
+@echo off
+SetLocal
+
+SET VERSION=0.9.4
+set DIR_ALL=%~dp0
+cd "%DIR_ALL%"
+
+java -jar "ddp2p_%VERSION%.jar"
+
+echo "Current Path: " %CD%
+
+call .\dd_install.bat
+
+set PREVIOUS="%DIR_ALL%..\PREVIOUS"
+
+rem set "RPATH=%DIR_ALL:~0,-1%"
+rem FOR /F (%RPATH%) set PREVIOUS=%dpi
+
+rem Next line has to be called even if %PREVIOUS% is not yet created
+rem IF EXIST %PREVIOUS% call .\unit_test.bat config.UnitSetImport "%PREVIOUS%"
+
+rem Will set importing indicators only if called without parameters
+SET /A ARGS_COUNT=0
+FOR %%A in (%*) DO SET /A ARGS_COUNT+=1
+
+IF %ARGS_COUNT% EQU 0 call .\unit_test.bat config.UnitSetImport "%PREVIOUS%"
+
+EndLocal
+
---
+++ updates/0.9.4/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Should be called with parameter to set it for importing
+
+VERSION=0.9.4
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR_ALL="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR_ALL="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "${DIR_ALL}"
+
+jar xvf "${DIR_ALL}/ddp2p_${VERSION}.jar"
+
+chmod a+x *.sh scripts/*sh
+
+./dd_install.sh
+
+
+PREVIOUS="$(dirname ${DIR_ALL})/PREVIOUS"
+### PREVIOUS MAY NOT YET HAVE BEEN CREATED, BUT HAS TO BE SET
+#if [ -e "${PREVIOUS}" ]
+if [ $# -eq 0 ]
+then
+	./unit_test.sh config.UnitSetImport "${PREVIOUS}"
+fi
+
